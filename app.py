06ba1a4b897d3e08e9dae92d327330ca4f68caae
Giancarlo Cavalli <giancarlo.cavalli-ext@viavarejo.com.br>
2022-10-06 16:16:06 -0300

    fix(api): fix endpoint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def pagina_inicial():
     return "Hello World"
 
 @app.route("/have-a-nice-day")
-def pagina_inicial():
+def pagina_greeting():
     return "Have a nice day!"
 
 if __name__ == '__main__':
